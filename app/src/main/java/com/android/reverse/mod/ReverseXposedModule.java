1c7612d2d5a8c6dd146ad224089ffcdd97e64588
banxi1988 <banxi1988@gmail.com>
2015-10-15 15:44:06 +0800

    添加依赖的 apache http 库，优化Xposed 模块入口代码的写法

--- app/src/main/java/com/android/reverse/mod/ReverseXposedModule.java
+++ app/src/main/java/com/android/reverse/mod/ReverseXposedModule.java
@@ -8,30 +8,32 @@ import com.android.reverse.collecter.LuaScriptInvoker;
 import com.android.reverse.collecter.ModuleContext;
 import com.android.reverse.util.Logger;
 
+
 import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.callbacks.XC_LoadPackage;
 import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
 
 public class ReverseXposedModule implements IXposedHookLoadPackage {
-
-	private static final String ZJDROID_PACKAGENAME = "com.android.reverse"; 
+	private static final String ZJDROID_PACKAGENAME = "com.android.reverse";
 	@Override
-	public void handleLoadPackage(LoadPackageParam lpparam) throws Throwable {
-		// TODO Auto-generated method stub
-		if(lpparam.appInfo == null || 
-				(lpparam.appInfo.flags & (ApplicationInfo.FLAG_SYSTEM | ApplicationInfo.FLAG_UPDATED_SYSTEM_APP)) !=0){
-			return;
-		}else if(lpparam.isFirstApplication && !ZJDROID_PACKAGENAME.equals(lpparam.packageName)){
-		  Logger.PACKAGENAME = lpparam.packageName;
-		  Logger.log("the package = "+lpparam.packageName +" has hook");
-		  Logger.log("the app target id = "+android.os.Process.myPid());
-		  PackageMetaInfo pminfo = PackageMetaInfo.fromXposed(lpparam);
-		  ModuleContext.getInstance().initModuleContext(pminfo);
-		  DexFileInfoCollecter.getInstance().start();
-		  LuaScriptInvoker.getInstance().start();
-		  ApiMonitorHookManager.getInstance().startMonitor();
-		}else{
-			
-		}
-	}
+	public void handleLoadPackage(LoadPackageParam loadPackageParam) throws Throwable {
+			if (loadPackageParam.appInfo == null){
+                return;
+            }
+            String applicationId = loadPackageParam.packageName;
+			boolean isSystemApp = (loadPackageParam.appInfo.flags & (ApplicationInfo.FLAG_SYSTEM | ApplicationInfo.FLAG_UPDATED_SYSTEM_APP)) != 0;
+			boolean isZjDroid = ZJDROID_PACKAGENAME.equals(applicationId);
+			if(isSystemApp || isZjDroid || !loadPackageParam.isFirstApplication){
+				return;
+			}
 
+            Logger.PACKAGENAME = applicationId;
+            Logger.log("the package = "+applicationId+" has hook");
+            Logger.log("the app target id = "+android.os.Process.myPid());
+            PackageMetaInfo pminfo = PackageMetaInfo.fromXposed(loadPackageParam);
+            ModuleContext.getInstance().initModuleContext(pminfo);
+            DexFileInfoCollecter.getInstance().start();
+            LuaScriptInvoker.getInstance().start();
+            ApiMonitorHookManager.getInstance().startMonitor();
+	}
 }
