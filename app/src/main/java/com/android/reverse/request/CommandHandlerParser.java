4222142bcfe62206e0f47343da7975f11309237b
banxi1988 <banxi1988@gmail.com>
2015-10-15 17:20:09 +0800

    重构 CommandHandlerParser 类

--- app/src/main/java/com/android/reverse/request/CommandHandlerParser.java
+++ app/src/main/java/com/android/reverse/request/CommandHandlerParser.java
@@ -28,56 +28,62 @@ public class CommandHandlerParser {
 	private static String FILE_SCRIPT = "filepath";
 
 	public static CommandHandler parserCommand(String cmd) {
-		CommandHandler handler = null;
 		try {
-			JSONObject jsoncmd = new JSONObject(cmd);
-			String action = jsoncmd.getString(ACTION_NAME_KEY);
-			Logger.log("the cmd = " + action);
-			if (ACTION_DUMP_DEXINFO.equals(action)) {
-				handler = new DumpDexInfoCommandHandler();
-			} else if (ACTION_DUMP_DEXFILE.equals(action)) {
-				if (jsoncmd.has(PARAM_DEXPATH_DUMPDEXCLASS)) {
-					String dexpath = jsoncmd.getString(PARAM_DEXPATH_DUMPDEXCLASS);
-					handler = new DumpDexFileCommandHandler(dexpath);
-				} else {
-					Logger.log("please set the " + PARAM_DEXPATH_DUMPDEXCLASS + " value");
-				}
-			} else if (ACTION_BACKSMALI_DEXFILE.equals(action)) {
-				if (jsoncmd.has(PARAM_DEXPATH_DUMPDEXCLASS)) {
-					String dexpath = jsoncmd.getString(PARAM_DEXPATH_DUMPDEXCLASS);
-					handler = new BackSmaliCommandHandler(dexpath);
-				} else {
-					Logger.log("please set the " + PARAM_DEXPATH_DUMPDEXCLASS + " value");
-				}
-			} else if (ACTION_DUMP_DEXCLASS.equals(action)) {
-				if (jsoncmd.has(PARAM_DEXPATH_DUMPDEXCLASS)) {
-					String dexpath = jsoncmd.getString(PARAM_DEXPATH_DUMP_DEXFILE);
-					handler = new DumpClassCommandHandler(dexpath);
-				} else {
-					Logger.log("please set the " + PARAM_DEXPATH_DUMPDEXCLASS + " value");
-				}
-			} else if (ACTION_DUMP_HEAP.equals(action)) {
-				handler = new DumpHeapCommandHandler();
-			} else if (ACTION_INVOKE_SCRIPT.equals(action)) {
-				if (jsoncmd.has(FILE_SCRIPT)) {
-					String filepath = jsoncmd.getString(FILE_SCRIPT);
-					handler = new InvokeScriptCommandHandler(filepath, ScriptType.FILETYPE);
-				} else {
-					Logger.log("please set the " + FILE_SCRIPT);
-				}
+			return lookupHandler(cmd);
 
-			} else if (ACTION_DUMP_MEMERY.equals(action)) {
-				int start = jsoncmd.getInt(PARAM_START_DUMP_MEMERY);
-				int length = jsoncmd.getInt(PARAM_LENGTH_DUMP_MEMERY);
-				handler = new DumpMemCommandHandler(start, length);
-			} else {
-				Logger.log(action + " cmd is invalid! ");
-			}
+		}catch (CommandParameterNotFoundException e){
+			Logger.log(e.getMessage());
+			Logger.log("Please set the "+e.parameterKey+" value");
 		} catch (JSONException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		return handler;
+		return null;
 	}
 
+
+	private static String getStringParameter(JSONObject json,String key) throws JSONException {
+		if(!json.has(key)){
+			throw new CommandParameterNotFoundException(key);
+		}
+		return json.getString(key);
+	}
+
+	private static CommandHandler lookupHandler(String cmd) throws JSONException{
+		JSONObject jsoncmd = new JSONObject(cmd);
+		String action = jsoncmd.getString(ACTION_NAME_KEY);
+		Logger.log("the cmd = " + action);
+		if (ACTION_DUMP_DEXINFO.equals(action)) {
+			return new DumpDexInfoCommandHandler();
+		} else if (ACTION_DUMP_DEXFILE.equals(action)) {
+			String dexpath = getStringParameter(jsoncmd,PARAM_DEXPATH_DUMPDEXCLASS);
+            return  new DumpDexFileCommandHandler(dexpath);
+		} else if (ACTION_BACKSMALI_DEXFILE.equals(action)) {
+			String dexpath = getStringParameter(jsoncmd,PARAM_DEXPATH_DUMPDEXCLASS);
+             return new BackSmaliCommandHandler(dexpath);
+		} else if (ACTION_DUMP_DEXCLASS.equals(action)) {
+            String dexpath = getStringParameter(jsoncmd,PARAM_DEXPATH_DUMP_DEXFILE);
+            return  new DumpClassCommandHandler(dexpath);
+		} else if (ACTION_DUMP_HEAP.equals(action)) {
+			return new DumpHeapCommandHandler();
+		} else if (ACTION_INVOKE_SCRIPT.equals(action)) {
+            String filepath = getStringParameter(jsoncmd,FILE_SCRIPT);
+            return  new InvokeScriptCommandHandler(filepath, ScriptType.FILETYPE);
+		} else if (ACTION_DUMP_MEMERY.equals(action)) {
+			int start = jsoncmd.getInt(PARAM_START_DUMP_MEMERY);
+			int length = jsoncmd.getInt(PARAM_LENGTH_DUMP_MEMERY);
+			return new DumpMemCommandHandler(start, length);
+		} else {
+			Logger.log(action + " cmd is invalid! ");
+		}
+		return null;
+	}
+
+}
+
+class CommandParameterNotFoundException extends IllegalArgumentException{
+	String parameterKey;
+	public CommandParameterNotFoundException(String parameterKey){
+		super("No Cmd Parameter For Key"+parameterKey);
+		this.parameterKey = parameterKey;
+	}
 }
