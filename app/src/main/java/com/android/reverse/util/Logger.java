54d82d37c0130c98d3a6c1e6c7310f8884c81069
banxi1988 <banxi1988@gmail.com>
2015-10-16 22:41:45 +0800

    修正 provided 使用错误 修改applicationId,暂不编译 jni 文件
    android gradle dsl 已经支持 provided scope了。
    增加自定义字段的 buildNumber

--- app/src/main/java/com/android/reverse/util/Logger.java
+++ app/src/main/java/com/android/reverse/util/Logger.java
@@ -2,21 +2,23 @@ package com.android.reverse.util;
 
 import android.util.Log;
 
+import com.android.reverse.BuildConfig;
+
 public class Logger {
 	
-	public static String LOGTAG_COMMAN = "zjdroid-shell-";
-	public static String LOGTAG_WORKFLOW = "zjdroid-apimonitor-";
+	public static String LOGTAG_COMMAN = "updroid"+BuildConfig.buildNumber+"-";
+	public static String LOGTAG_WORKFLOW = "updroid"+BuildConfig.buildNumber+"-apimonitor-";
 	public static boolean DEBUG_ENABLE = true;
 	public static String PACKAGENAME;
 	
 	public static void log(String message){
 		if(DEBUG_ENABLE)
-			Log.d(LOGTAG_COMMAN+PACKAGENAME,message);
+			Log.i(LOGTAG_COMMAN+PACKAGENAME,message);
 	}
 	
 	public static void log_behavior(String message){
 		if(DEBUG_ENABLE)
-			Log.d(LOGTAG_WORKFLOW+PACKAGENAME,message);
+			Log.i(LOGTAG_WORKFLOW+PACKAGENAME,message);
 	}
 	
 }
