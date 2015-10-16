60f09483b439e7685fde03baf5a581d348895244
banxi1988 <banxi1988@gmail.com>
2015-10-16 10:58:58 +0800

    重构 NetworkHook 类

--- app/src/main/java/com/android/reverse/apimonitor/NetWorkHook.java
+++ app/src/main/java/com/android/reverse/apimonitor/NetWorkHook.java
@@ -9,11 +9,12 @@ import org.apache.http.HttpEntity;
 import org.apache.http.HttpHost;
 import org.apache.http.HttpRequest;
 import org.apache.http.HttpResponse;
-import org.apache.http.client.methods.HttpGet;
+import org.apache.http.RequestLine;
 import org.apache.http.client.methods.HttpPost;
-import org.apache.http.client.utils.URLEncodedUtils;
 import org.apache.http.protocol.HTTP;
 import org.apache.http.protocol.HttpContext;
+import org.apache.http.util.EntityUtils;
+
 import com.android.reverse.hook.HookParam;
 import com.android.reverse.util.Logger;
 import com.android.reverse.util.RefInvoke;
@@ -23,116 +24,68 @@ public class NetWorkHook extends ApiMonitorHook {
 
 	@Override
 	public void startHook() {
-		// TODO Auto-generated method stub
-		// HttpURLConnection
+		hookHttpURLConnection();
+		hookHttpClient();
+	}
+
+	private void hookHttpURLConnection(){
 		Method openConnectionMethod = RefInvoke.findMethodExact("java.net.URL", ClassLoader.getSystemClassLoader(), "openConnection");
 		hookhelper.hookMethod(openConnectionMethod, new AbstractBahaviorHookCallBack() {
 			@Override
 			public void descParam(HookParam param) {
-				// TODO Auto-generated method stub
 				URL url = (URL) param.thisObject;
 				Logger.log_behavior("Connect to URL ->");
 				Logger.log_behavior("The URL = " + url.toString());
 			}
 		});
+	}
 
+	private void hookHttpClient(){
 		Method executeRequest = RefInvoke.findMethodExact("org.apache.http.impl.client.AbstractHttpClient", ClassLoader.getSystemClassLoader(),
 				"execute", HttpHost.class, HttpRequest.class, HttpContext.class);
 
 		hookhelper.hookMethod(executeRequest, new AbstractBahaviorHookCallBack() {
 			@Override
 			public void descParam(HookParam param) {
-				// TODO Auto-generated method stub
 				Logger.log_behavior("Connect to URL ->");
 				HttpHost host = (HttpHost) param.args[0];
 				HttpRequest request = (HttpRequest) param.args[1];
-				if (request instanceof HttpGet) {
-					HttpGet httpGet = (HttpGet) request;
-					Logger.log_behavior("HTTP Method : " + httpGet.getMethod());
-					Logger.log_behavior("HTTP URL : " + httpGet.getURI().toString());
-					Header[] headers = request.getAllHeaders();
-					if (headers != null) {
-						for (int i = 0; i < headers.length; i++) {
-							Logger.log_behavior(headers[i].getName() + ":" + headers[i].getName());
-						}
-					}
-				} else if (request instanceof HttpPost) {
-					HttpPost httpPost = (HttpPost) request;
-					Logger.log_behavior("HTTP Method : " + httpPost.getMethod());
-					Logger.log_behavior("HTTP URL : " + httpPost.getURI().toString());
-					Header[] headers = request.getAllHeaders();
-					if (headers != null) {
-						for (int i = 0; i < headers.length; i++) {
-							Logger.log_behavior(headers[i].getName() + ":" + headers[i].getValue());
-						}
-					}
-					HttpEntity entity = httpPost.getEntity();
-					String contentType = null;
-					if (entity.getContentType() != null) {
-						contentType = entity.getContentType().getValue();
-						if (URLEncodedUtils.CONTENT_TYPE.equals(contentType)) {
-
-							try {
-								byte[] data = new byte[(int) entity.getContentLength()];
-								entity.getContent().read(data);
-								String content = new String(data, HTTP.DEFAULT_CONTENT_CHARSET);
-								Logger.log_behavior("HTTP POST Content : " + content);
-							} catch (IllegalStateException e) {
-								// TODO Auto-generated catch block
-								e.printStackTrace();
-							} catch (IOException e) {
-								// TODO Auto-generated catch block
-								e.printStackTrace();
-							}
-
-						} else if (contentType.startsWith(HTTP.DEFAULT_CONTENT_TYPE)) {
-							try {
-								byte[] data = new byte[(int) entity.getContentLength()];
-								entity.getContent().read(data);
-								String content = new String(data, contentType.substring(contentType.lastIndexOf("=") + 1));
-								Logger.log_behavior("HTTP POST Content : " + content);
-							} catch (IllegalStateException e) {
-								// TODO Auto-generated catch block
-								e.printStackTrace();
-							} catch (IOException e) {
-								// TODO Auto-generated catch block
-								e.printStackTrace();
-							}
-
-						}
-					}else{
-						byte[] data = new byte[(int) entity.getContentLength()];
-						try {
-							entity.getContent().read(data);
-							String content = new String(data, HTTP.DEFAULT_CONTENT_CHARSET);
-							Logger.log_behavior("HTTP POST Content : " + content);
-						} catch (IllegalStateException e) {
-							// TODO Auto-generated catch block
-							e.printStackTrace();
-						} catch (IOException e) {
-							// TODO Auto-generated catch block
-							e.printStackTrace();
-						}
-
+				RequestLine requestLine = request.getRequestLine();
+				String method = requestLine.getMethod();
+				boolean isGET = "GET".equalsIgnoreCase(method);
+				boolean isPOST = "POST".equalsIgnoreCase(method);
+				if(isGET || isPOST){
+					Logger.log_behavior("HTTP Method : " + method);
+					Logger.log_behavior("HTTP URL : " + requestLine.getUri());
+					logHeaders(request.getAllHeaders());
+				}
+				if (isPOST) {
+					HttpEntity entity = ((HttpPost)request).getEntity();
+					try {
+						String content = EntityUtils.toString(entity, HTTP.DEFAULT_CONTENT_CHARSET);
+						Logger.log_behavior("HTTP POST Content : " + content);
+					}catch (IOException e){
+						e.printStackTrace();
 					}
 
 				}
 			}
 
+			private void logHeaders(Header[] headers){
+				if (headers != null) {
+					for (int i = 0; i < headers.length; i++) {
+						Logger.log_behavior(headers[i].getName() + ":" + headers[i].getValue());
+					}
+				}
+			}
+
 			@Override
 			public void afterHookedMethod(HookParam param) {
-				// TODO Auto-generated method stub
 				super.afterHookedMethod(param);
 				HttpResponse resp = (HttpResponse) param.getResult();
 				if (resp != null) {
 					Logger.log_behavior("Status Code = " + resp.getStatusLine().getStatusCode());
-					Header[] headers = resp.getAllHeaders();
-					if (headers != null) {
-						for (int i = 0; i < headers.length; i++) {
-							Logger.log_behavior(headers[i].getName() + ":" + headers[i].getValue());
-						}
-					}
-
+					logHeaders(resp.getAllHeaders());
 				}
 			}
 		});
